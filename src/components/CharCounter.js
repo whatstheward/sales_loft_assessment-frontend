7700595412700af2005d7de33fe59e23a4e6002b
Maddie <whatstheward@gmail.com>
2019-09-22 17:16:38 -0400

    build chararacter counter container

---
+++ src/components/CharCounter.js
@@ -0,0 +1,24 @@
+import React from 'react';
+const CharCounter = (props) => {
+    
+            return(
+                <div>
+                    <table>
+                        <thead>
+                            <tr>
+                                <td>
+                                    Character
+                                </td>
+                                <td>
+                                    Count
+                                </td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        </tbody>
+                    </table>
+                </div>
+            )
+}
+
+export default CharCounter
